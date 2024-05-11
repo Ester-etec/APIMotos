c73f35cbefa43da83b5fab9fb76497ab0c91dc3c
Ester-etec <estersilvasantos.etec@gmail.com>
2024-05-10 21:30:11 -0300

    controller da API

--- APIMotos10/app/Http/Controllers/MotoController.php
+++ APIMotos10/app/Http/Controllers/MotoController.php
@@ -53,24 +53,64 @@ class MotoController extends Controller
     /**
      * Display the specified resource.
      */
-    public function show(moto $moto)
+    public function show(string $id)
     {
-        //
+        $moto = moto::find($id);
+
+        if($moto){
+
+            return 'Moto localizda'.$moto.response()->json([],Response::HTTP_NO_CONTENT);
+        } else {
+
+            return 'Moto não localizda'.Response()->json([],Response::HTTP_NO_CONTENT);
+        }
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, moto $moto)
+    public function update(Request $request, string $id)
     {
-        //
+        $dadosMotos = $request->All();
+        $validarDados = Validator::make($dadosMotos,[
+            'marca' => 'required',
+            'modelo' => 'required',
+            'cor' => 'required',
+            'ano' => 'required',
+        ]);
+ 
+        if ($validarDados->fails()) {
+            return 'Dados invalidos'.$validarDados->error(true). 500;
+        }
+
+        $moto = moto::find($id);
+        $moto->marca = $dadosMotos['marca'];
+        $moto->modelo = $dadosMotos['modelo'];
+        $moto->cor = $dadosMotos['cor'];
+        $moto->ano = $dadosMotos['ano'];
+
+        $retorno = $moto->save();
+
+        if($retorno){
+
+            return 'Dados atualizados com sucesso'.Response()->json([],Response::HTTP_NO_CONTENT);
+        } else {
+
+            return 'Dados não atualizados'.Response()->json([],Response::HTTP_NO_CONTENT);
+        }
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(moto $moto)
+    public function destroy(string $id)
     {
-        //
+        $dadosMotos = moto::find($id);
+
+        if($dadosMotos->delete()){
+            return 'O veículo foi deletado com sucesso!!'.response()->json([],Response::HTTP_NO_CONTENT);
+        } 
+
+        return 'O veículo não foi deletado'.response()->json([],Response::HTTP_NO_CONTENT);
     }
 }
--- APIMotos10/routes/api.php
+++ APIMotos10/routes/api.php
@@ -4,7 +4,14 @@ use App\Http\Controllers\MotoController;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Http\Response;
 
-Route::get('/', function () {
-    return 'APi de motos'.Response()->json([],Response::HTTP_NO_CONTENT);});
+Route::get('/', function () {return 'APi de motos'.Response()->json(['Sucesso'=>true]);});
+
 Route::get('/motos',[MotoController::class,'index']);
-Route::post('/motoscadastra',[MotoController::class,'store']);
+
+Route::post('/motos',[MotoController::class,'store']);
+
+Route::delete('/motos/{id}',[MotoController::class,'destroy']);
+
+Route::put('/motos/{id}',[MotoController::class,'update']);
+
+Route::get('/motos/{id}',[MotoController::class,'show']);
